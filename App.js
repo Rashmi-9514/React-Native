56910946fda9d02059309739b49b6b8310965db6
Rashmi-9514 <rashmi@clevertap.com>
2021-03-10 11:54:33 +0530

    App inbox

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
+import {   
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
   StatusBar,
-} from 'react-native';
+  Button,
+  Alert} from 'react-native';
 
 import {
   Header,
@@ -23,10 +16,120 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+const CleverTap = require('clevertap-react-native');
 
-const App: () => React$Node = () => {
-  return (
-    <>
+
+export default class App extends React.Component {
+	 
+	 displayAlert = () => {
+			alert('I am an alert for on button click');
+  
+		//console.log('Display on called: ', res, err);
+			
+		
+		//Show Inbox 
+		CleverTap.showInbox({'tabs':['Offers','Promotions'],'navBarTitle':'My App Inbox','navBarTitleColor':'#FF0000','navBarColor':'#FFFFFF','inboxBackgroundColor':'#AED6F1','backButtonColor':'#00FF00'
+                                ,'unselectedTabColor':'#0000FF','selectedTabColor':'#FF0000','selectedTabIndicatorColor':'#000000',
+                                'noMessageText':'No message(s)','noMessageTextColor':'#FF0000'});
+								
+								
+		}; 		
+   
+	 componentDidMount() {
+		 
+		CleverTap.addListener(CleverTap.CleverTapProfileSync, (event) => { this._handleCleverTapEvent(CleverTap.CleverTapProfileSync, event); });
+		
+     
+		CleverTap.setDebugLevel(10);
+		
+		//Setting up a Push Notification
+		CleverTap.setPushToken("111056687894", CleverTap.FCM);
+			
+		//Recording an Event	
+		CleverTap.recordEvent('testEvent');
+        
+		CleverTap.recordEvent('testEventWithProps', {'foo': 'bar'});
+        if (Platform.OS === 'android') {
+             CleverTap.setPushToken("111056687894", CleverTap.FCM);
+        }
+    
+		//On user Login 
+		CleverTap.onUserLogin({'Name': 'v', 'Identity': '45671890', 'Email': 'v@gmail.com', 'custom1': 13});
+
+		//Updating user profile after user logs in.
+		CleverTap.profileSet({"Identity":45671890, "Name":"V","Email":"v@gmail.com","Gender":"Male","DOB":"1995-10-14", "custom":1.73});
+		 
+            
+		//Setting Multiple Values
+		CleverTap.profileSetMultiValuesForKey(['a', 'b', 'c'], 'letters');
+        
+		//Adding Multiple Value (Single Value)
+		CleverTap.profileAddMultiValueForKey('d', 'letters');
+        
+		//Adding Multiple Value (Multiple Values)
+		CleverTap.profileAddMultiValuesForKey(['e', 'f'], 'letters');
+        
+		//Removing a Value from the Multiple Values
+		CleverTap.profileRemoveMultiValueForKey('b', 'letters');
+        
+		//Removing Multiple Values
+		CleverTap.profileRemoveMultiValuesForKey(['a', 'c'], 'letters');
+		
+		CleverTap.profileAddMultiValuesForKey(["V1","V2"],"Values");
+        
+		//Set Location
+		CleverTap.setLocation(34.15, -118.20);
+    
+		//Recording the charged events
+		CleverTap.recordChargedEvent({'totalValue': 20, 'category': 'books'}, [{'title': 'book1'}, {'title': 'book2'}, {'title': 'book3'}]);
+	
+		//Creating Notification Channel
+		 CleverTap.createNotificationChannel("RNTesting", "React Native Testing", "React Native Testing", 4, true);
+		 
+		//initialize the App Inbox
+        CleverTap.initializeInbox();  
+		
+		//{this.displayAlert}				
+		
+		CleverTap.getInboxMessageCount((err, res) => {
+				console.log('Total Messages: ', res, err);
+			});
+		CleverTap.getInboxMessageUnreadCount((err, res) => {
+				console.log('Unread Messages: ', res, err);
+			});
+		
+
+		CleverTap.getAllInboxMessages((err, res) => {
+				console.log('All Inbox Messages: ', res, err);
+			 });
+		
+
+		CleverTap.getUnreadInboxMessages((err, res) => {
+				 console.log('Unread Inbox Messages: ', res, err);
+			 });
+		
+		CleverTap.getInboxMessageForId('Message Id',(err, res) => {
+            console.log("marking message read = "+res);
+        });
+
+        CleverTap.deleteInboxMessageForId('Message Id');
+        CleverTap.markReadInboxMessageForId('Message Id');
+        CleverTap.pushInboxNotificationViewedEventForId('Message Id');
+        CleverTap.pushInboxNotificationClickedEventForId('Message Id');			
+
+		
+	}
+	 
+	 componentWillUnmount() {
+		 CleverTap.removeListener(CleverTap.CleverTapProfileSync);
+		 
+	 }
+	 
+	
+       
+   render() {
+      return (
+         <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView
@@ -46,7 +149,7 @@ const App: () => React$Node = () => {
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionTitle}>Which Is Good</Text>
               <Text style={styles.sectionDescription}>
                 <ReloadInstructions />
               </Text>
@@ -58,16 +161,22 @@ const App: () => React$Node = () => {
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Learn More</Text>
               <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
+                Click on the Button to Open Inbox
               </Text>
             </View>
-            <LearnMoreLinks />
+             <View style={{ marginTop: 100 }}>
+			<Button title="Click Here" color="blue" onPress={this.displayAlert} />
+
+        
+      </View>
           </View>
         </ScrollView>
       </SafeAreaView>
     </>
-  );
-};
+      );
+   }
+}
+
 
 const styles = StyleSheet.create({
   scrollView: {
@@ -107,5 +216,3 @@ const styles = StyleSheet.create({
     textAlign: 'right',
   },
 });
-
-export default App;
--- index.js
+++ index.js
@@ -9,6 +9,51 @@ import {DeviceEventEmitter, NativeEventEmitter, NativeModules} from 'react-nativ
 
 const CleverTapReact = NativeModules.CleverTapReact;
 const EventEmitter = NativeModules.CleverTapReactEventEmitter ? new NativeEventEmitter(NativeModules.CleverTapReactEventEmitter) : DeviceEventEmitter;
+const CleverTap = require('clevertap-react-native');
+
+
+
+
+//CleverTap.setDebugLevel(3);
+ 
+
+
+CleverTap.recordEvent('testEvent');
+
+	//User Properties
+		//Creating User profile when the user logs in for first time
+	//	CleverTap.onUserLogin({'Name': 'P', 'Identity': '45213', 'Email': 'p@gmail.com', 'custom1': 173});
+
+		//Updating user profile after user logs in.
+	//	CleverTap.profileSet({"Identity":347687, "Name":"P","Email":"p@gmail.com","Gender":"Male","DOB":"1995-10-15", "custom":1.3});
+		   
+		//Setting multiple Values for keys profiles   
+	//	CleverTap.profileSetMultiValuesForKey(['V1', 'V2', 'V3'], 'Many Values');
+		
+		//Adding Multiple values for keys
+	//	 CleverTap.profileAddMultiValuesForKey(['V1', 'V2'], 'Many Values');
+
+		//removing multiple profiles
+	//	CleverTap.profileRemoveMultiValueForKey('b', 'letters');
+
+		//Adding location
+	//	CleverTap.setLocation(34.15, -118.20);
+		
+		
+		
+	//Enables Push Notifications   
+	//CleverTap.setPushToken("FCM-Token", CleverTap.FCM);
+    
+	//Create notification channel for Android O and above
+	//CleverTap.createNotificationChannel("RNTesting", "React Native Testing", "React Native Testing", 4, true);
+	//initialize the App Inbox
+	//CleverTap.initializeInbox();
+	
+	
+
+
+
+
 
 function defaultCallback(method, err, res) {
     if (err) {
@@ -16,12 +61,7 @@ function defaultCallback(method, err, res) {
     } else {
         console.log('CleverTap ' + method + ' default callback result', res);
     }
-}
-
-
-
-
-
+};
 
 
 AppRegistry.registerComponent(appName, () => App);
